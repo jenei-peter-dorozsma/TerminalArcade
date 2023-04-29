9d8729c288ca102e7e4f9f43c9de3581db4e790d
Peter Jenei <peter.jenei@pronovix.com>
2023-04-29 15:15:33 +0200

    Added license information

--- TerminalGames/Games/Assets/handArt.py
+++ TerminalGames/Games/Assets/handArt.py
@@ -1,4 +1,18 @@
 '''Ascii art for hand signs'''
+# Copyright 2023 Peter Jenei
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 HAND_ART = {
     'Paper': [
         '    _______        ',
@@ -68,7 +82,78 @@ HAND_ART = {
         '        (___)      ',
         '___     (__)___    ',
         '   \___________)   '
-   ]
+   ],
+
+   'Point': [
+        '    _______        ',
+        '___/   ____)______ ',
+        '          ________)',
+        '        (___)      ',
+        '___     (___)      ',
+        '   \___(___)       ',
+        '                   '
+   ],
+
+   'Gun': [
+        '     __            ',
+        '    │  │           ',
+        '___/   │__________ ',
+        '          ________)',
+        '        (___)      ',
+        '___     (___)      ',
+        '   \___(___)       '
+   ],
+
+   'One': [
+        '     __            ',
+        '    │  │           ',
+        '___/   │_____      ',
+        '        (____)     ',
+        '        (____)     ',
+        '___     (___)      ',
+        '   \___(___)       '
+    ],
+
+    'Two': [
+        '     __            ',
+        '    │  │           ',
+        '___/   │__________ ',
+        '          ________)',
+        '        (___)      ',
+        '___     (___)      ',
+        '   \___(___)       '
+   ],
+
+   'Three': [
+        '     __            ',
+        '    │  │           ',
+        '___/   │________   ',
+        '          ______)_ ',
+        '          ________)',
+        '___     (___)      ',
+        '   \___(___)       '
+    ],
+
+    'Four': [
+        '     __            ',
+        '    │  │           ',
+        '___/   │________   ',
+        '          ______)_ ',
+        '          ________)',
+        '___       ______)  ',
+        '   \___(___)       '
+    ],
+
+    'Five': [
+        '     __            ',
+        '    │  │           ',
+        '___/   │________   ',
+        '          ______)_ ',
+        '          ________)',
+        '___       ______)  ',
+        '   \__________)    '
+    ],
+
 }
 
 HAND_HEIGHT = len(HAND_ART['Rock'])
